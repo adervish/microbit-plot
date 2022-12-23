8c8974a13347e5ceb48ae3ed65c492f3d456fabc
Alex Derbes <xor@bentboolean.com>
2022-12-23 11:10:46 -0800

    Ash added text

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
+let myX = 0
+let myY = 0
 joystickbit.initJoystickBit()
+joystickbit.Vibration_Motor(100)
 basic.forever(function () {
-    basic.clearScreen()
-    led.plot(Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.X) / 250), Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.Y) / 250))
-    serial.writeNumbers([joystickbit.getRockerValue(joystickbit.rockerType.X), joystickbit.getRockerValue(joystickbit.rockerType.Y)])
-    basic.pause(100)
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        basic.showString("Hello!")
+        basic.showString("my name is bit boy.")
+    } else {
+        basic.clearScreen()
+        myY = Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.Y) / 250)
+        myX = Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.X) / 250)
+        led.plot(myX, myY)
+        serial.writeNumbers([joystickbit.getRockerValue(joystickbit.rockerType.X), joystickbit.getRockerValue(joystickbit.rockerType.Y)])
+    }
 })
