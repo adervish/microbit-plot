e5c397fe05be166c8f02ba24efddd9fe4dd2b963
Alex Derbes <xor@bentboolean.com>
2022-12-25 05:24:14 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,41 @@
+radio.onReceivedValue(function (name, value) {
+    if (value == 1) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    }
+    if (value == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+    }
+})
+function getX () {
+    return Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.Y) / 250)
+}
 let myX = 0
 let myY = 0
+radio.setGroup(1)
 joystickbit.initJoystickBit()
 joystickbit.Vibration_Motor(100)
 basic.forever(function () {
-    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
-        basic.showString("Hello!")
-        basic.showString("my name is bit boy.")
-    } else {
-        basic.clearScreen()
-        myY = Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.Y) / 250)
-        myX = Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.X) / 250)
-        led.plot(myX, myY)
-        serial.writeNumbers([joystickbit.getRockerValue(joystickbit.rockerType.X), joystickbit.getRockerValue(joystickbit.rockerType.Y)])
+    basic.clearScreen()
+    myY = getX()
+    myX = Math.round(4 - joystickbit.getRockerValue(joystickbit.rockerType.X) / 250)
+    if (getX() > 2) {
+        radio.sendValue("forword", 1)
     }
+    if (getX() < 2) {
+        radio.sendValue("back", 2)
+    }
+    led.plot(myX, myY)
+    serial.writeNumbers([joystickbit.getRockerValue(joystickbit.rockerType.X), joystickbit.getRockerValue(joystickbit.rockerType.Y)])
 })
